19b06f5cb50ab88a6fa26c65a678c231bb7b3d14
connormckey <connormckey@gmail.com>
2025-03-25 16:31:26 -0500

    basic express server

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,9 +1,28 @@
 const express = require('express')
+
+
 const app = express()
 const port = 3001
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/example-json-endpoint', (req, res) => {
+  const data = {
+    message: "hello",
+    itemsInCart: [
+      "hammer",
+      "bowling ball"
+    ]
+  }
+  res.json(data)
+})
+
+app.post('/mirror', (req, res) => {
+  console.log(req.body);      // your JSON
+  res.send(req.body);
 })
 
 app.listen(port, () => {
